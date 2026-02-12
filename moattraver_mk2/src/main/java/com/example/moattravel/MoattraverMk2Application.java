098ee5515fa89f0f63cd9d1d361e24775f071021
satsuki <satsuki@DESKTOP-RKSUEQT>
2026-02-12 18:46:55 +0900

    aa

---
+++ moattraver_mk2/src/main/java/com/example/moattravel/MoattraverMk2Application.java
@@ -0,0 +1,13 @@
+package com.example.moattravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoattraverMk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoattraverMk2Application.class, args);
+	}
+
+}
